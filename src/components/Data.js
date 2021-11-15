59bcd16d02739353d131956b4765e2cd1acfa3bd
Gore Auluck <32429409+GSometimes@users.noreply.github.com>
2021-11-15 00:51:56 -0800

    first commit

---
+++ src/components/Data.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+
+const Data = () => {
+  const [coronavirusData, setCoronavirusData] = useState([]);
+
+  const getCoronavirusData = async () => {
+    const response = await fetch("http://localhost:8080/data");
+    const data = await response.json();
+    setCoronavirusData(data);
+  };
+
+  useEffect(() => {
+    getCoronavirusData();
+  }, []);
+
+  return (
+    <div className="flex flex-row flex-wrap justify-center items-center">
+      {coronavirusData.map((currentData) => {
+        return (
+          <div className="flex flex-col justify-evenly items-center mb-2 max-w-sm rounded overflow-hidden shadow-md p-6 h-40 w-40 text-center font-mono m-2">
+            {/* <p className="text-gray-700">{currentData?.state}</p> */}
+            <p className="text-gray-700">{currentData?.country}</p>
+            <p className="text-gray-700">
+              Total Cases: {currentData?.latestTotalCases}
+            </p>
+            <p className="text-gray-700">
+              Increase: {currentData?.diffFromPrevDay}
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Data;
---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <h1 className="font-mono">This is the Hero Section</h1>
+    </div>
+  );
+};
+
+export default Hero;
