0a46e1d202f886f3b96f24da32083d1b260c37de
Gore Auluck <32429409+GSometimes@users.noreply.github.com>
2021-11-25 22:56:26 -0800

    started hero style update

--- src/components/Data.js
+++ src/components/Data.js
@@ -7,7 +7,10 @@ const Data = ({ data }) => {
     <div className="flex flex-row flex-wrap justify-center items-center">
       {data.map((currentData, idx) => {
         return (
-          <div className="flex flex-col justify-evenly items-center mb-2 max-w-sm rounded overflow-hidden shadow-md p-5 h-52 w-80 md:h-56 md:w-56 lg:h-72 lg:w-72 text-center font-mono m-2 bg-blue-dark" key={currentData.country + idx}>
+          <div
+            className="flex flex-col justify-evenly items-center mb-2 max-w-sm rounded overflow-hidden shadow-md p-5 h-52 w-80 md:h-56 md:w-56 lg:h-72 lg:w-72 text-center font-mono m-2 bg-blue-dark"
+            key={currentData.country + idx}
+          >
             <p className="text-yellow-light">{currentData?.country}</p>
             <p className="text-yellow-light">
               Total Cases:{" "}
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,27 +3,27 @@ import Search from "./Search";
 
 const Hero = ({ data, setFilteredData }) => {
   return (
-    <div className="w-full h-96 flex flex-col items-center justify-center font-mono font-bold">
-      <div className=" flex flex-col items-center justify-center w-5/6 h-5/6 rounded bg-blue-dark shadow:md px-10">
-        <h1 className="text-4xl md:text-5xl lg:text-7xl text-yellow-light m-5 text-center">
+    // <div className="w-full h-screen flex flex-col items-center justify-center font-mono font-bold">
+    //   <div className=" flex flex-col items-center justify-center w-5/6 h-5/6 rounded bg-blue-dark shadow:md px-10">
+    //     <h1 className="text-4xl md:text-5xl lg:text-7xl text-yellow-light m-5 text-center">
+    //       Coronavirus Tracker
+    //     </h1>
+    //     <p className="text-sm md:text-base lg:text-lg text-yellow-light text-center">
+    //       This application lists the current number of cases reported across the
+    //       globe and the total updates daily.
+    //     </p>
+
+    //     <div className="mt-10">
+    //       <Search data={data} setFilteredData={setFilteredData} />
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="h-screen w-full flex">
+      <div className=" w-full background-image flex justify-end items-center font-bold">
+        <h1 className="text-4xl text-yellow-light md:text-5xl lg:text-7xl m-20 text-center">
           Coronavirus Tracker
         </h1>
-        <p className="text-sm md:text-base lg:text-lg text-yellow-light text-center">
-          This application lists the current number of cases reported across the
-          globe and the total updates daily.
-        </p>
-
-        <div className="mt-10">
-          <Search data={data} setFilteredData={setFilteredData} />
-        </div>
       </div>
-      {/* <div className="absolute bottom-20 h-36 flex flex-col justify-evenly items-center mt-5">
-        <p className="text-sm md:text-base text-gray-700">Scroll to View Data</p>
-        <img
-          src="https://img.icons8.com/ios/75/000000/long-arrow-down.png"
-          alt=""
-        />
-      </div> */}
     </div>
   );
 };
