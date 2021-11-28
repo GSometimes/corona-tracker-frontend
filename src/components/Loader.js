595f90c0498c2660b4c584cd2403703b16c1dbf1
Gore Auluck <32429409+GSometimes@users.noreply.github.com>
2021-11-27 21:56:45 -0800

    Updated Loader

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Loader = () => {
-  let circleCommonClasses = "h-2.5 w-2.5 bg-black rounded-full";
+  let circleCommonClasses = "h-5 w-5 bg-black rounded-full";
   return (
-    <div className="flex m-10">
-      <div className={`${circleCommonClasses} mr-1 animate-bounce`}></div>
-      <div className={`${circleCommonClasses} mr-1 animate-bounce200`}></div>
-      <div className={`${circleCommonClasses} mr-1 animate-bounce400`}></div>
+    <div className="flex items-center justify-center m-10">
+      <div className={`${circleCommonClasses} mr-2 animate-bounce`}></div>
+      <div className={`${circleCommonClasses} mr-2 animate-bounce200`}></div>
+      <div className={`${circleCommonClasses} animate-bounce400`}></div>
     </div>
   );
 };
